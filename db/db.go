5eee976a9afa82b2eaf18aee8bfd37f436abf42b
knqyf263 <knqyf263@gmail.com>
2017-10-12 13:46:50 +0900

    Support redis as DB back end for RedHat

--- db/db.go
+++ db/db.go
@@ -27,6 +27,8 @@ func NewDB(dbType string) (DB, error) {
 	switch dbType {
 	case dialectSqlite3, dialectMysql, dialectPostgreSQL:
 		return &RDBDriver{name: dbType}, nil
+	case dialectRedis:
+		return &RedisDriver{name: dbType}, nil
 	}
 	return nil, fmt.Errorf("Invalid database dialect. err: %s", dbType)
 }
