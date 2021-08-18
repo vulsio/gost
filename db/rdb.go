5be75462cc5b4c43bdd6d0001fe584bdce18496f
MaineK00n <mainek00n.1229@gmail.com>
2021-08-19 01:59:34 +0900

    chore: print gorm log to stderr

--- db/rdb.go
+++ db/rdb.go
@@ -46,7 +46,12 @@ func (r *RDBDriver) Name() string {
 func (r *RDBDriver) OpenDB(dbType, dbPath string, debugSQL bool) (locked bool, err error) {
 	gormConfig := gorm.Config{
 		DisableForeignKeyConstraintWhenMigrating: true,
-		Logger:                                   logger.Default.LogMode(logger.Silent),
+		Logger: logger.New(
+			log.New(os.Stderr, "\r\n", log.LstdFlags),
+			logger.Config{
+				LogLevel: logger.Silent,
+			},
+		),
 	}
 
 	if debugSQL {
