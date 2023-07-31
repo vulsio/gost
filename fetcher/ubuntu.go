f914b239d3770b34638bebe4d244f32244516502
MaineK00n <mainek00n.1229@gmail.com>
2023-07-31 11:29:22 +0900

    fix(fetcher/ubuntu): temporarily use vuln-list-reserve (#205)

--- fetcher/ubuntu.go
+++ fetcher/ubuntu.go
@@ -15,14 +15,14 @@ import (
 )
 
 const (
-	ubuntuRepoURL = "https://github.com/aquasecurity/vuln-list.git"
+	ubuntuRepoURL = "https://github.com/aquasecurity/vuln-list-reserve.git"
 	ubuntuDir     = "ubuntu"
 )
 
 // FetchUbuntuVulnList clones vuln-list and returns CVE JSONs
 func FetchUbuntuVulnList() (entries []models.UbuntuCVEJSON, err error) {
 	// Clone vuln-list repository
-	dir := filepath.Join(util.CacheDir(), "vuln-list")
+	dir := filepath.Join(util.CacheDir(), "vuln-list-reserve")
 	updatedFiles, err := git.CloneOrPull(ubuntuRepoURL, dir, ubuntuDir)
 	if err != nil {
 		return nil, xerrors.Errorf("error in vulnsrc clone or pull: %w", err)
